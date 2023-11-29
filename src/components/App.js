ebbf731db1bc3d90c7455b6fdf51422261e62e0e
Richard Barbour <richyb07@gmail.com>
2023-11-29 10:04:08 -0800

    Add square component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Square from './Square';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Square.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Square() {
+  return <button className="square">1</button>
+}
+
+export default Square;
